d4bb9e297bef539b6af4d5ae18aa93253dbe3224
Filipe Silva <140545021+filipesilva9225@users.noreply.github.com>
2026-05-06 21:03:05 +0000

    adding all files

---
+++ github_finder/src/components/Search.tsx
@@ -0,0 +1,32 @@
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>;
+};
+
+import { useState } from "react";
+
+import { BsSearch } from "react-icons/bs";
+
+function Search({ loadUser }: SearchProps) {
+  const [userName, setUserName] = useState("");
+
+  return (
+    <>
+      <div>
+        <h2>Busque por um usuário:</h2>
+        <p>Conheça seus melhores repositórios</p>
+        <div>
+          <input
+            type="text"
+            onChange={(e) => setUserName(e.target.value)}
+            placeholder="Digite o nome do usuário"
+          />
+          <button onClick={() => loadUser(userName)}>
+            <BsSearch />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Search;
